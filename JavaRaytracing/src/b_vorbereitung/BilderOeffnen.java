107381a6d1024ae4c66552f48c2083d295187a06
Benj_S <bschurian4@gmail.com>
2015-10-27 19:03:39 +0100

    2 Zeilen geschrieben

---
+++ JavaRaytracing/src/b_vorbereitung/BilderOeffnen.java
@@ -0,0 +1,12 @@
+package b_vorbereitung;
+
+import javax.swing.JFileChooser;
+
+public class BilderOeffnen {
+	public static void main(String[]args){
+		JFileChooser chooser=new JFileChooser();
+		chooser.showOpenDialog(null);
+	}
+	
+	
+}
