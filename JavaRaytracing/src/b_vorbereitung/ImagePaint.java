1766495b5c39fc57e99ee47679a81b17c04a45fa
Clemens <clemens_pade@web.de>
2015-11-06 22:30:22 +0100

    Zwischenergebnis ein paar Sachen fehlen noch

---
+++ JavaRaytracing/src/b_vorbereitung/ImagePaint.java
@@ -0,0 +1,41 @@
+package b_vorbereitung;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JPanel;
+
+public class ImagePaint extends JPanel{
+	
+private BufferedImage image;
+				
+	public void paintImage(){
+		int w = this.getWidth();
+		int h = this.getHeight();
+		
+		final int black = new Color(0,0,0).getRGB();
+		final int red = new Color(255,0,0).getRGB();
+		this.image = new BufferedImage(w,h,BufferedImage.TYPE_INT_RGB);	
+	
+		for(int i = 0; i<w; i++){
+			
+			for(int j=0; j<h; j++){
+				
+				if(i==j){
+					image.setRGB(i, j, red);
+				}
+				else { 
+					image.setRGB(i, j, black);
+				}
+			}
+		}				
+	}
+	
+	protected void paintComponent( Graphics g ){
+		paintImage();
+	    if ( image != null )
+	      g.drawImage( image, 0, 0, this );
+	  }
+	
+}
