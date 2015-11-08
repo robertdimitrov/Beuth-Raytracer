d8821e35fe19f0e8a017f87aeb66e78d938f9ee9
Clemens <clemens_pade@web.de>
2015-11-08 20:51:04 +0100

    ImagePaint und ImageTest aktualisiert

--- JavaRaytracing/src/b_vorbereitung/ImageTest.java
+++ JavaRaytracing/src/b_vorbereitung/ImageTest.java
@@ -1,10 +1,14 @@
 package b_vorbereitung;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
-import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+
 
 public class ImageTest {
 
@@ -12,14 +16,23 @@ public class ImageTest {
 		
 		JFrame frame = new JFrame("Imageviewer");		
 		frame.setLayout( new BorderLayout() );	
-		JMenuBar menu = new JMenuBar();
-		JMenu save = new JMenu("save");
-		menu.add(save);
+		JMenuBar menu = new JMenuBar();				
 		frame.setJMenuBar(menu);
 		ImagePaint imagePainter = new ImagePaint();
+		ActionListener listener = new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e){
+				System.out.println("hallo");
+				 imagePainter.save();
+			}
+		};
+		JMenuItem save = new JMenuItem("save");
+		save.addActionListener(listener);
+		menu.add(save);
 		frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
 	    frame.setSize( 640, 480 );
 	    frame.setVisible( true );
 	    frame.add(imagePainter);
+	   
 	}
 }
