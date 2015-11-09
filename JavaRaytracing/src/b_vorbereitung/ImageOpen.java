5e0aefc4d04c7207aa85e079755258ddd47a20c6
Benj_S <bschurian4@gmail.com>
2015-11-09 20:12:04 +0100

    alles jetzt static

--- JavaRaytracing/src/b_vorbereitung/ImageOpen.java
+++ JavaRaytracing/src/b_vorbereitung/ImageOpen.java
@@ -16,71 +16,67 @@ import javax.swing.filechooser.FileFilter;
 
 public class ImageOpen {
 	public static void main(String[] args) {
-		// Schedule a job for the event-dispatching thread:
-		// creating and showing this application's GUI.
-		javax.swing.SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				final JFileChooser fChooser = new JFileChooser();
-				//hinzufuegen eines FileFilters, der nur Bilder als öffnenbare Dateien anzeigt
-				fChooser.setFileFilter(new FileFilter() {
+		final JFileChooser fChooser = new JFileChooser();
+		// hinzufuegen eines FileFilters, der nur Bilder als öffnenbare Dateien
+		// anzeigt
+		fChooser.setFileFilter(new FileFilter() {
 
-					@Override
-					public String getDescription() {
-						return "images( jpg, bmp, gif, png, jpeg, wbmp)";
-					}
-					
-					@Override
-					public boolean accept(File f) {
-						if (f.isDirectory()) {
-							return true;
-						}
-						for (String s : ImageIO.getReaderFileSuffixes()) {
-							if (f.toString().endsWith("." + s)) {
-								return true;
-							}
-						}
-						return false;
-					}
-				});
-
-				int returnVal = fChooser.showOpenDialog(null);
-				if (returnVal == JFileChooser.APPROVE_OPTION) {
-					showImageFile(fChooser.getSelectedFile());
-				}
+			@Override
+			public String getDescription() {
+				return "images( jpg, bmp, gif, png, jpeg, wbmp)";
 			}
 
-			/**
-			 * Zeigt eine als Bild in einem eigenen Fenster an
-			 * 
-			 * @param file die Datei die angezeigt werden soll
-			 */
-			private void showImageFile(final File file ) {
-				final JFrame imgframe = new JFrame();
-				//try-catch-Block für den Fall das ImageIO file nicht lesen kann
-				try {
-					final BufferedImage img = ImageIO.read(file);
-					imgframe.getContentPane().add(new JPanel() {
-						/**
-						 * 
-						 */
-						private static final long serialVersionUID = 1L;
-
-						@Override
-						public void paintComponent(Graphics g) {
-							BufferedImage image = img;
-							Graphics2D graphics2d = (Graphics2D) g;
-							graphics2d.drawImage(image, 0, 0, null);
-							super.paintComponents(g);
-						}	
-					});
-					imgframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-					imgframe.setSize(img.getWidth(), img.getHeight());
-					imgframe.setVisible(true);
-				} catch (IOException e) {
-					System.err.println("IOException ist aufgetreten: "+file + " konnte nicht gelesen werden");
+			@Override
+			public boolean accept(File f) {
+				if (f.isDirectory()) {
+					return true;
 				}
+				for (String s : ImageIO.getReaderFileSuffixes()) {
+					if (f.toString().endsWith("." + s)) {
+						return true;
+					}
+				}
+				return false;
 			}
 		});
+
+		int returnVal = fChooser.showOpenDialog(null);
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+			showImageFile(fChooser.getSelectedFile());
+		}
 	}
 
+	/**
+	 * Zeigt eine als Bild in einem eigenen Fenster an
+	 * 
+	 * @param file
+	 *            die Datei die angezeigt werden soll
+	 */
+	private static void showImageFile(final File file) {
+		final JFrame imgframe = new JFrame();
+		// try-catch-Block für den Fall das ImageIO file nicht lesen kann
+		try {
+			final BufferedImage img = ImageIO.read(file);
+			imgframe.getContentPane().add(new JPanel() {
+				/**
+						 * 
+						 */
+				private static final long serialVersionUID = 1L;
+
+				@Override
+				public void paintComponent(Graphics g) {
+					BufferedImage image = img;
+					Graphics2D graphics2d = (Graphics2D) g;
+					graphics2d.drawImage(image, 0, 0, null);
+					super.paintComponents(g);
+				}
+			});
+			imgframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			imgframe.setSize(img.getWidth(), img.getHeight());
+			imgframe.setVisible(true);
+		} catch (IOException e) {
+			System.err.println("IOException ist aufgetreten: " + file
+					+ " konnte nicht gelesen werden");
+		}
+	};
 }
