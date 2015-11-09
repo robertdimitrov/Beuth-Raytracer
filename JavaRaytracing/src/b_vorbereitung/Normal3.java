38edc284dcb53ae1dca8013a87a1f19d64e9c3a3
Dimitrov, Robert <s59246@beuth-hochschule.de>
2015-11-09 12:48:57 +0100

    Klasse dokumentiert

--- JavaRaytracing/src/b_vorbereitung/Normal3.java
+++ JavaRaytracing/src/b_vorbereitung/Normal3.java
@@ -1,25 +1,62 @@
 package b_vorbereitung;
 
+/**
+ * Diese Klasse stellt eine Normale (x,y,z) auf einer Oberfläche dar.
+ * @author Kosmonaut
+ *
+ */
 public class Normal3 {
 	
+	/**
+	 * Der x-Wert der Normalen.
+	 */
 	public final double x;
+	
+	/**
+	 * Der y-Wert der Normalen.
+	 */
 	public final double y;
+	
+	/**
+	 * Der z-Wert der Normalen.
+	 */
 	public final double z;
-
+	
+	/**
+	 * Dieser Konstruktor erzeugt ein neues Normal3-Objekt
+	 * @param x der x-Wert der Normalen
+	 * @param y der y-Wert der Normalen
+	 * @param z der z-Wert der Normalen
+	 */
 	public Normal3(final double x, final double y, final double z){
 		this.x = x;
 		this.y = y;
 		this.z = z;
 	}
 	
+	/**
+	 * Multipliziert diese Normal3 mit einer reellen Zahl.
+	 * @param n der Multiplikator
+	 * @return das Produkt der Normalen und der Zahl
+	 */
 	public Normal3 mul(double n){
 		return new Normal3(n*x, n*y, n*z);
 	}
 	
+	/**
+	 * Addiert diese Normal3 mit einer anderen Normal3.
+	 * @param n die zweite Normale
+	 * @return die Summe beider Normalen
+	 */
 	public Normal3 add(Normal3 n){
 		return new Normal3(x + n.x, y + n.y, z + n.z);
 	}
 	
+	/**
+	 * Berechnet das Skalarprodukt dieser Normal3 und einem Vector3.
+	 * @param v der Vektor
+	 * @return das Skalarprodukt der Normalen und des Vektors
+	 */
 	public double dot(Vector3 v){
 		return x*v.x + y*v.y + z*v.z;
 	}
