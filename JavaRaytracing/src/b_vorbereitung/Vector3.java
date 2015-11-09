87c0451fbe9a59704b8d8196f115ffb36adf958d
Dimitrov, Robert <s59246@beuth-hochschule.de>
2015-11-09 03:04:18 +0100

    hashCode() equals() toString()

--- JavaRaytracing/src/b_vorbereitung/Vector3.java
+++ JavaRaytracing/src/b_vorbereitung/Vector3.java
@@ -61,5 +61,49 @@ public class Vector3 {
 	public Vector3 x(Vector3 v){
 		return new Vector3(y*v.z - z*v.y, z*v.x - x*v.z, x*v.y - y*v.x);
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(magnitude);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(x);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(y);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(z);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Vector3 other = (Vector3) obj;
+		if (Double.doubleToLongBits(magnitude) != Double
+				.doubleToLongBits(other.magnitude))
+			return false;
+		if (Double.doubleToLongBits(x) != Double.doubleToLongBits(other.x))
+			return false;
+		if (Double.doubleToLongBits(y) != Double.doubleToLongBits(other.y))
+			return false;
+		if (Double.doubleToLongBits(z) != Double.doubleToLongBits(other.z))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Vector3 [x=" + x + ", y=" + y + ", z=" + z + ", magnitude="
+				+ magnitude + "]";
+	}
+
 	
 }
