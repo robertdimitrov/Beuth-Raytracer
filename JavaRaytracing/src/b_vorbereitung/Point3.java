3037d95009e40c333ae87f0df7d7d9c1f1e4bb6b
Dimitrov, Robert <s59246@beuth-hochschule.de>
2015-11-09 12:44:10 +0100

    Klasse dokumentiert

--- JavaRaytracing/src/b_vorbereitung/Point3.java
+++ JavaRaytracing/src/b_vorbereitung/Point3.java
@@ -1,25 +1,63 @@
 package b_vorbereitung;
 
+/**
+ * Diese Klasse stellt einen Punkt (x,y,z) im 3D-Raum dar.
+ * @author Kosmonaut
+ *
+ */
 public class Point3 {
 	
+	/**
+	 * Der x-Wert des Punktes.
+	 */
 	public final double x;
+	
+	/**
+	 * Der y-Wert des Punktes.
+	 */
 	public final double y;
+	
+	/**
+	 * Der z-Wert des Punktes.
+	 */
 	public final double z;
 	
+	
+	/**
+	 * Dieser Konstruktor erzeugt ein neues Point3-Objekt.
+	 * @param x Der x-Wert des Punktes.
+	 * @param y Der y-Wert des Punktes.
+	 * @param z Der z-Wert des Punktes.
+	 */
 	public Point3(final double x, final double y, final double z){
 		this.x = x;
 		this.y = y;
 		this.z = z;
 	}
 	
+	/**
+	 * Subtrahiert einen Point3 von diesem Point3.
+	 * @param p der Subtrahend
+	 * @return die Differenz zwischen den beiden Punkten
+	 */
 	public Vector3 sub(Point3 p){
 		return new Vector3(x - p.x, y - p.y, z - p.z);
 	}
 	
+	/**
+	 * Subtrahiert einen Vector3 von diesem Point3.
+	 * @param v der Subtrahend
+	 * @return die Differenz zwischen dem Punkt und dem Vektor
+	 */
 	public Point3 sub(Vector3 v){
 		return new Point3(x - v.x, y - v.y, z - v.z);
 	}
 	
+	/**
+	 * Addiert diesen Point3 mit einem Vector3.
+	 * @param v der Vektor
+	 * @return die Summe des Punktes und des Vektors
+	 */
 	public Point3 add(Vector3 v){
 		return new Point3(x + v.x, y + v.y, z + v.z);
 	}
