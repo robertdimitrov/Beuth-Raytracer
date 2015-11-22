c4f4d2d5553e72ed7d1fca807733e3ccc96cb82a
Dimitrov, Robert <s59246@beuth-hochschule.de>
2015-11-22 21:18:29 +0100

    rayFor

--- JavaRaytracing/src/aufgabe01/PerspectiveCamera.java
+++ JavaRaytracing/src/aufgabe01/PerspectiveCamera.java
@@ -16,7 +16,7 @@ public class PerspectiveCamera extends Camera {
 	public Ray rayFor(final int w, final int h, final int x, final int y) {
 		final Vector3 vectorX = u.mul(x - ((double)(w-1)/2));
 		final Vector3 vectorY = v.mul(y - ((double)(h-1)/2));
-		final Vector3 r = this.w.mul(((double)h/2)/Math.tan(angle)).mul(-1).add(vectorX).add(vectorX);
+		final Vector3 r = this.w.mul(((double)h/2)/Math.tan(angle)).mul(-1).add(vectorX).add(vectorY);
 		
 		return new Ray(e, r.normalized());
 	}
