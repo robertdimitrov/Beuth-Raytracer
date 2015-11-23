8ac1be9d09bf6dd1276f6070896e6834fc6ae695
Dimitrov, Robert <s59246@beuth-hochschule.de>
2015-11-23 16:46:40 +0100

    Persp.Camera kommentiert

--- JavaRaytracing/src/aufgabe01/PerspectiveCamera.java
+++ JavaRaytracing/src/aufgabe01/PerspectiveCamera.java
@@ -3,19 +3,34 @@ package aufgabe01;
 import b_vorbereitung.Point3;
 import b_vorbereitung.Vector3;
 
+/**
+ * Diese Klasse stellt eine perspektivische Kamera dar
+ * @author Kosmonaut
+ *
+ */
 public class PerspectiveCamera extends Camera {
 
+	/**
+	 * Der Öffnungswinkel der Kamera
+	 */
 	public final double angle;
 	
-	public PerspectiveCamera(Point3 e, Vector3 g, Vector3 t, double angle) {
+	/**
+	 * Dieser Konstruktor erzeugt ein neues PerspectiveCamera-Objekt
+	 * @param e Position der Kamera
+	 * @param g Blickrichtung der Kamera
+	 * @param t Up-Vektor der Kamera
+	 * @param angle Öffnungswinkel der Kamera
+	 */
+	public PerspectiveCamera(final Point3 e, final Vector3 g, final Vector3 t, final double angle) {
 		super(e, g, t);
 		this.angle = angle;
 	}
 
 	@Override
 	public Ray rayFor(final int w, final int h, final int x, final int y) {
-		final Vector3 vectorX = u.mul(x - ((double)(w-1)/2));
-		final Vector3 vectorY = v.mul(y - ((double)(h-1)/2));
+		final Vector3 vectorX = u.mul(x - (((double)w-1)/2));
+		final Vector3 vectorY = v.mul(y - (((double)h-1)/2));
 		final Vector3 r = this.w.mul(((double)h/2)/Math.tan(angle)).mul(-1).add(vectorX).add(vectorY);
 		
 		return new Ray(e, r.normalized());
