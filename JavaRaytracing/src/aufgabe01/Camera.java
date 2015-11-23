36562d39a3c521aca7d262cff3a29d7f7ea05f61
Dimitrov, Robert <s59246@beuth-hochschule.de>
2015-11-23 16:14:23 +0100

    Camera kommentiert

--- JavaRaytracing/src/aufgabe01/Camera.java
+++ JavaRaytracing/src/aufgabe01/Camera.java
@@ -3,25 +3,64 @@ package aufgabe01;
 import b_vorbereitung.Point3;
 import b_vorbereitung.Vector3;
 
+/**
+ * Diese abstrakte Klasse stellt eine Kamera dar.
+ * @author Kosmonaut
+ *
+ */
 public abstract class Camera {
 
+	/**
+	 * Die Position "e" der Kamera.
+	 */
 	public final Point3 e;
+	/**
+	 * Die Blickrichtung (gaze direction) "d" der Kamera.
+	 */
 	public final Vector3 g;
+	/**
+	 * Der Up-Vektor "t" der Kamera.
+	 */
 	public final Vector3 t;
+	/**
+	 * Die u-Achse der Kamera.
+	 */
 	public final Vector3 u;
+	/**
+	 * Die v-Achse der Kamera.
+	 */
 	public final Vector3 v;
+	/**
+	 * Die w-Achse der Kamera.
+	 */
 	public final Vector3 w;
 	
+	/**
+	 * Dieser Konstruktor erzeugt ein neues Camera-Objekt.
+	 * Die u-, v- und w-Achsen der Kamera werden im Konstruktor berechnet und 
+	 * als öffentliche Attribute zur Verfügung gestellt.
+	 * @param e Die Position der Kamera
+	 * @param g Die Blickrichtung der Kamera
+	 * @param t Der Up-Vektor der Kamera
+	 */
 	public Camera(final Point3 e, final Vector3 g, final Vector3 t){
 		this.e = e;
 		this.g = g;
 		this.t = t;
 		
-		w = g.normalized().mul(-1);
-		u = t.x(g).normalized();
+		w = g.mul(-1).normalized();
+		u = (t.x(g)).normalized();
 		v = w.x(u);
 	}
 	
+	/**
+	 * Gibt für einen bestimmten Pixel den entsprechenden Strahl zurück.
+	 * @param w Breite des Bildes
+	 * @param h Höhe des Bildes
+	 * @param x x-Koordinate des Pixels
+	 * @param y y-Koordinate des Pixels
+	 * @return Der entsprechende Strahl
+	 */
 	public abstract Ray rayFor(final int w, final int h, final int x, final int y);
 
 	@Override
