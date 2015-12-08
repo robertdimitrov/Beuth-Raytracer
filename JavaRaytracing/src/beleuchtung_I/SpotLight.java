1fd8e6692ae3723555136539d76b307e8de5302d
Benj_S <bschurian4@gmail.com>
2015-12-08 18:19:59 +0100

    fehlerhaft

--- JavaRaytracing/src/beleuchtung_I/SpotLight.java
+++ JavaRaytracing/src/beleuchtung_I/SpotLight.java
@@ -1,6 +1,7 @@
 package beleuchtung_I;
 
 import aufgabe01.Color;
+import aufgabe01.World;
 import b_vorbereitung.Point3;
 import b_vorbereitung.Vector3;
 
@@ -39,21 +40,24 @@ public class SpotLight extends Light {
 	 *            Eine Haelfte des Oeffnungswinkels des Lichts
 	 */
 	public SpotLight(final Color color, final Point3 position,
-			final Vector3 direction, final double halfAngle) {
-		super(color);
+			final Vector3 direction, final double halfAngle, final boolean castsShadow) {
+		super(color, castsShadow);
 		this.position = position;
 		this.direction = direction;
 		this.halfAngle = halfAngle;
 	}
 
 	@Override
-	public boolean illuminates(Point3 point) {
+	public boolean illuminates(Point3 point, World world) {
+		if(!castsShadow){
+			return point.sub(position).angleTo(direction) <= halfAngle;
+		}
 		return point.sub(position).angleTo(direction) <= halfAngle;
 	}
 
 	@Override
 	public Vector3 directionFrom(Point3 point) {
-		return position.sub(point);
+		return position.sub(point).normalized();
 	}
 
 }
